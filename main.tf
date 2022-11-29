28ee5acab9bc1240a0a35dfdc6dea36c3127a7d5
tamtmh <tampv@tmh-techlab.vn>
2022-11-29 17:03:57 +0700

    create before destroy

--- main.tf
+++ main.tf
@@ -16,4 +16,8 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ansible_server" {
   ami           = data.aws_ami.ami.id
   instance_type = "t3.small"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
